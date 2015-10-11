a6587544f6a078231d63ed1699b030a8a3d91bfc
BarmanInfo <barmaninfo1991@gmail.com>
2015-10-11 17:50:24 +0530

    Commit 10th October 2015

--- gen/com/gpstracker/R.java
+++ gen/com/gpstracker/R.java
@@ -2104,7 +2104,8 @@ containing a value of this type.
         public static final int select_dialog_multichoice_material=0x7f030022;
         public static final int select_dialog_singlechoice_material=0x7f030023;
         public static final int showlocation=0x7f030024;
-        public static final int support_simple_spinner_dropdown_item=0x7f030025;
+        public static final int splash=0x7f030025;
+        public static final int support_simple_spinner_dropdown_item=0x7f030026;
     }
     public static final class menu {
         public static final int splash=0x7f0c0000;
